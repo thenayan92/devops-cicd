48d64b7a346bf956f9ce874df0c42cb62856061b
root <root@ip-172-31-28-20.ap-south-1.compute.internal>
2026-02-04 19:20:28 +0000

    Test mango auto deploy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello via Webhook UPDATED from Jenkins + Docker + Kubernetes 🚀"
+    return "Hello via mango via Webhook UPDATED from Jenkins + Docker + Kubernetes 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
